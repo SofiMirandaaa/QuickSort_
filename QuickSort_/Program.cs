8b560e77cac1cfe33bf19884d0fb8e1f05edeccd
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-10-21 17:44:08 +0700

    add  q_sort(j + 1, high)

--- QuickSort_/Program.cs
+++ QuickSort_/Program.cs
@@ -102,9 +102,7 @@ namespace QuickSort_
             q_sort (low, j - 1);
 
             //sort the list on the right of pivot using quick sort
-
-
-
+            q_sort(j + 1, high);
 
         }
 
