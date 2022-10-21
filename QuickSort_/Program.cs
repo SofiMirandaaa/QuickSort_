8c656b8822d55fa36d5d833109355b206694a529
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-10-21 17:49:23 +0700

    add for (int j = 0; j < n; j++)

--- QuickSort_/Program.cs
+++ QuickSort_/Program.cs
@@ -110,6 +110,12 @@ namespace QuickSort_
             Console.WriteLine("\n-------------------------");
             Console.WriteLine("Sorted array elements");
             Console.WriteLine("-------------------------");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr [j]);
+            }
+            
         }
 
     }
